15b31be28cc38d91f20e24ae66a4f1e99d15ef66
ferhatclk <59114579+ferhatclk@users.noreply.github.com>
2022-08-21 17:05:31 +0300

    Add MyDictionary Project

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    
+    class MyDictionary <K,V>
+    {
+        KeyValuePair<K, V>[] items;
+        public MyDictionary()
+        {
+            items = new KeyValuePair<K, V>[0];
+
+        }
+
+        public void Add(K key, V value)
+        {
+            if (CheckIfKeyExist(key))
+            {
+                KeyValuePair<K, V>[] tempArray = items;
+
+                items = new KeyValuePair<K, V>[items.Length + 1];
+
+                for (int i = 0; i < tempArray.Length; i++)
+                {
+                    items[i] = tempArray[i];
+                }
+
+                items[items.Length - 1] = new KeyValuePair<K, V>(key, value);
+            }
+
+        }
+
+        private bool CheckIfKeyExist(K key)
+        {
+            bool result = true;
+            foreach (var item in items)
+            {
+                if (item.Key.Equals( key))
+                {
+                    result = false;
+                }
+                    
+            }
+            return result; 
+        }
+
+        public int Count
+        {
+            get { return items.Length; }
+        }
+
+
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int,string> cities = new Dictionary <int,string> ();
+            
+            cities.Add(06, "Ankara");
+            cities.Add(01, "Adana");
+            
+            Console.WriteLine(cities.Count);
+            
+
+
+            MyDictionary<int, string> cities2 = new MyDictionary<int, string>();
+            
+            cities2.Add(75, "Ardahan");
+            cities2.Add(76, "Iğdır");
+            
+            Console.WriteLine(cities2.Count);
+           
+        }
+
+
+    }
+}
